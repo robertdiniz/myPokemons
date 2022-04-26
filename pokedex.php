2399d799ff75fa75f069b77f8b3db3af6675dc10
robertdiniz <dinizrobert2002@gmail.com>
2022-04-26 17:00:24 -0300

    Consumir API

--- pokedex.php
+++ pokedex.php
@@ -19,6 +19,12 @@ $resultado = mysqli_query($connect, $sql);
 $dados = mysqli_fetch_array($resultado);
 
 mysqli_close($connect);
+
+// API
+$url = "https://www.canalti.com.br/api/pokemons.json";
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$pokemons = json_decode(curl_exec($ch));
 ?>
 <div id="black-screen">
     <main class="login-div">
@@ -32,15 +38,27 @@ mysqli_close($connect);
                 </ul>
             </nav>
 
-            <div class="pokedex-container">
-                <div class="pokedex-pokemon">
-                    <img src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/258.png" width="100px" class="pokedex-poke">
-                    <h3>Pokémon</h3>
-                    <p>TIPO</p>
-                </div>
+            <div class="pokedex-container d-flex flex-column justify-content-center align-items-center">
+                <?php
+                // SE HOUVER BUSCA...
+                if (isset($_GET['search'])) :
+                    $poke = $_GET['poke'];
+                    // FAZER UM FOREACH PARA MOSTRAR O RESULTADO
+                    foreach ($pokemons->pokemon as $Pokemon) {
+                        if ($poke == $Pokemon->name) { ?>
+                            <div class="card-body d-flex flex-column justify-content-center align-items-center">
+                                <img src="<?= $Pokemon->img ?>" alt="<?= $Pokemon->name ?>" width="120px">
+                                <h4 class="card-title text-center"><?= $Pokemon->name ?></h2>
+                            </div>
+                <?php }
+                    }
+                endif; ?>
+                <form action="" method="get" class="mb-3">
+                    <input type="text" placeholder="Busque seu Pokémon..." name="poke">
+                    <button name="search">Buscar</button>
+                </form>
             </div>
-
-            <a href="logout.php" class="btn btn-danger">Sair</a>
+            <a href=" logout.php" class="btn btn-danger">Sair</a>
         </div>
     </main>
 </div>
